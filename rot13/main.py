1a2a44de89bcabf0da122bb907bd5a9c498e8097
Mendoza <camendoza@DIRECTV.com>
2016-09-23 01:01:29 -0700

    testing changes

--- rot13/main.py
+++ rot13/main.py
@@ -18,6 +18,9 @@ import os
 import webapp2
 import jinja2
 
+template_dir = os.path.join(os.path.dirname(__file__),"templates")
+jinja_env = jinja2.FileSystemLoader(template_dir)
+
 form = """
     <h1>Enter some text to ROT13:</h1>
     <form method="post" action="/rot13">
@@ -25,10 +28,19 @@ form = """
         <input type="submit">
     </form>
 """
+class TemplateHandler(webapp2.RequestHandler):
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+    def render_str(self, template, **params):
+        t = jinja_env.get_template(template)
+        return t.render(params)
+    def render(self, template, **kw)
+        self.write(self.render_str(template, **kw))
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
+        textarea = self.request.get_all("text")
         self.response.out.write(form)
     def post(self):
         text = self.request.get("text")
